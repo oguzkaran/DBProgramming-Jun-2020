8a92f734c9322519cc1428e49e7af465ea70c89e
Oğuz Karan <oguzkaran@csystem.org>
2021-01-08 21:42:38 +0300

    DBProgramming-Jun-2020

--- src/sample.sql
+++ src/sample.sql
@@ -1,5 +1,11 @@
 ﻿/*----------------------------------------------------------------------------------------------------------------------	
-	
+	insert into select cümlesi ile bir tabloya başka bir tablodan ekleme yapılabilir. Bu durumda karşılıklı eklenen 
+	elemanların uygun şekilde olması gerekir. Şüphesiz hedef tablonun varolması gerekir. Aşağıdaki örnekte 
+	questions_backup tablosunun kendine ait bir identity primary key bilgisi olduğundan question_id kaynak
+	tabloadan alınmamıştır
 ----------------------------------------------------------------------------------------------------------------------*/
 
-backup database competitionappdb to disk='C:\Users\Karans\Dropbox\CSD\Kurslar\DBProgramming-Jun-2020\src\Databases\MSSQL\competitionappdb.bak'
+insert into questions_backup (description, level_id, answer_index) 
+(select description, level_id, answer_index from questions)
+
+select * from questions_backup
